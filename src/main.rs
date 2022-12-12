0e11a490ae68bbe3e824269ea38ea88e9151c5b1
ArkV1 <Ark.V@posteo.net>
2022-12-12 13:20:35 +0200

    HTTPS only crate changed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use actix_web::{get, App, HttpRequest, HttpServer, Responder};
+use actix_web_middleware_redirect_scheme::RedirectSchemeBuilder;
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-//use actix_web_middleware_redirect_https::RedirectHTTPS;
-
 
 #[get("/")]
 async fn index(_req: HttpRequest) -> impl Responder {
@@ -22,16 +21,16 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             // HTTPS ONLY
-            // .wrap(RedirectHTTPS::with_replacements(&[(
-            //     ":8080".to_owned(),
-            //     ":8443".to_owned(),
-            // )]))
+            .wrap(
+                RedirectSchemeBuilder::new()
+                    .temporary() // 307 Temporary Redirect
+                    .replacements(&[(":8080", ":8443")]) // !!!TESTING PORTS!!!
+                    .build(),
+            )
             .service(index)
     })
-    .bind("127.0.0.1:8080")? // Testing port
-    //.bind("127.0.0.1:80")? // Production
-    .bind_openssl("127.0.0.1:8443", builder)? // Testing port - SECURED
-    //.bind_openssl("127.0.0.1:443", builder)? // Production port - SECURED
+    .bind("127.0.0.1:8080")? // Testing port :8080 | Production port :80 - UNSECURED
+    .bind_openssl("127.0.0.1:8443", builder)? // Testing port :8443 | Production port :443 - SECURED
     .run()
     .await
 }
