4a4a27eaf6297003ab3d33bb3443466797685928
ArkV1 <Ark.V@posteo.net>
2022-12-12 13:10:51 +0200

    HTTPS only

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, App, HttpRequest, HttpServer, Responder};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+//use actix_web_middleware_redirect_https::RedirectHTTPS;
+
 
 #[get("/")]
 async fn index(_req: HttpRequest) -> impl Responder {
@@ -17,9 +19,19 @@ async fn main() -> std::io::Result<()> {
         .unwrap();
     builder.set_certificate_chain_file("cert.pem").unwrap();
 
-    HttpServer::new(|| App::new().service(index))
-        .bind_openssl("127.0.0.1:8080", builder)?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            // HTTPS ONLY
+            // .wrap(RedirectHTTPS::with_replacements(&[(
+            //     ":8080".to_owned(),
+            //     ":8443".to_owned(),
+            // )]))
+            .service(index)
+    })
+    .bind("127.0.0.1:8080")? // Testing port
+    //.bind("127.0.0.1:80")? // Production
+    .bind_openssl("127.0.0.1:8443", builder)? // Testing port - SECURED
+    //.bind_openssl("127.0.0.1:443", builder)? // Production port - SECURED
+    .run()
+    .await
 }
-
