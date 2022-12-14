e805d4431c3f401aa16c59103f07ef5ed685dc4e
ArkV1 <Ark.V@posteo.net>
2022-12-14 10:25:12 +0200

    first working template

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 // ACTIX
-use actix_web::{get, App, HttpRequest, HttpServer, Responder};
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 // HTTPS
 use actix_web_middleware_redirect_scheme::RedirectSchemeBuilder;
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 // TEMPLATING
-use tera::Tera;
+use tera::{Tera, Context};
+use lazy_static::lazy_static;
 // LOCALS
-use unic_langid::{LanguageIdentifier, langid};
 use fluent_templates::{FluentLoader, static_loader};
+use unic_langid::{LanguageIdentifier, langid};
+
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let mut tera = match Tera::new("templates/**/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        //tera.autoescape_on(vec![".html", ".sql"]);
+        tera.autoescape_on(vec![]);
+        tera.register_function("fluent", FluentLoader::new(&*LOCALES));
+        tera
+    };
+}
 
 const US_ENGLISH: LanguageIdentifier = langid!("en-US");
+const RUSSIAN: LanguageIdentifier = langid!("ru");
 
 static_loader! {
     // Declare our `StaticLoader` named `LOCALES`.
@@ -22,9 +41,11 @@ static_loader! {
         core_locales: "./locales/core.ftl",
     };
 }
-#[get("/")]
-async fn index(_req: HttpRequest) -> impl Responder {
-    "Welcome!"
+
+async fn index(_req: HttpRequest) -> impl Responder  {
+    let mut context = Context::new();
+    context.insert("name", "Artyom");
+    HttpResponse::Ok().body(TEMPLATES.render("index.html", &context).unwrap())
 }
 
 #[actix_web::main]
@@ -38,10 +59,6 @@ async fn main() -> std::io::Result<()> {
         .unwrap();
     builder.set_certificate_chain_file("cert.pem").unwrap();
 
-    let mut tera = tera::Tera::default();
-    let ctx = tera::Context::default();
-    tera.register_function("fluent", FluentLoader::new(&*LOCALES));
-    
     HttpServer::new(|| {
         App::new()
             // HTTPS ONLY
@@ -51,8 +68,7 @@ async fn main() -> std::io::Result<()> {
                     .replacements(&[(":8080", ":8443")]) // !!!TESTING PORTS!!!
                     .build(),
             )
-            .service(index)
-    })
+            .service(web::resource("/").route(web::get().to(index)))    })
     .bind("127.0.0.1:8080")? // Testing port :8080 | Production port :80 - UNSECURED
     .bind_openssl("127.0.0.1:8443", builder)? // Testing port :8443 | Production port :443 - SECURED
     .run()
